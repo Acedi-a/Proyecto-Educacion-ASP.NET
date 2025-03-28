10b224fa61f9d5b1904908b056b057a9e0790a57
Josema-montano <montanojm336@gmail.com>
2025-03-27 21:29:21 -0300

    a

---
+++ ProyectoEducacion/Modelos/Pago.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoEducacion.Modelos
+{
+    public class Pago
+    {
+
+        [Key]
+        public int IdPago { get; set; }
+
+        [Required]
+        public float Monto { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Estado { get; set; }
+
+        [Required]
+        public DateTime Fecha { get; set; }
+
+        [Required]
+        public int IdEstudiante { get; set; }
+    }
+}
