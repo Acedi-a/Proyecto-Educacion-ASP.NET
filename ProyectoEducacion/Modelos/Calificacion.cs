639d061f8b109703ef9d56a48cbe1445a57812fd
Acedi-a <stardarck15@gmail.com>
2025-03-27 21:54:06 -0400

    se agregan clase, asistencia, calificacion, certificado, horario, materia, mensaje

---
+++ ProyectoEducacion/Modelos/Calificacion.cs
@@ -0,0 +1,40 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoEducacion.Modelos
+{
+    public class Calificacion
+    {
+        [Key]
+        public int IdCalificacion { get; set; }
+
+        [Required(ErrorMessage = "La nota es requerida")]
+        [Range(0, 100, ErrorMessage = "La nota debe estar entre 0 y 100")]
+        public decimal Nota { get; set; }
+
+        [StringLength(500, ErrorMessage = "Los comentarios no pueden exceder los 500 caracteres")]
+        public string Comentarios { get; set; }
+
+        [Required(ErrorMessage = "El ID del estudiante es requerido")]
+        public int IdEstudiante { get; set; }
+
+        [Required(ErrorMessage = "El ID del examen es requerido")]
+        public int IdExamen { get; set; }
+
+        [Required(ErrorMessage = "El ID del curso es requerido")]
+        public int IdCurso { get; set; }
+
+        [Required(ErrorMessage = "La fecha de registro es requerida")]
+        [DataType(DataType.Date)]
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+
+        [ForeignKey(nameof(IdEstudiante))]
+        public Estudiante Estudiante { get; set; }
+
+        [ForeignKey(nameof(IdExamen))]
+        public Examen Examen { get; set; }
+
+        [ForeignKey(nameof(IdCurso))]
+        public Curso Curso { get; set; }
+    }
+}
---
+++ ProyectoEducacion/Modelos/Horario.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoEducacion.Modelos
+{
+    public class Horario
+    {
+        [Key]
+        public int idHorario { get; set; }
+
+        [Required(ErrorMessage = "El campo diaSemana es obligatorio")]
+        [StringLength(15, ErrorMessage = "El dia de la semana no cumple con los requerimientos", MinimumLength = 3)]
+        public string diaSemana { get; set; }
+
+        [Required(ErrorMessage = "El campo hora de inicio es obligatorio")]
+        [DataType(DataType.Time)]
+        public DateTime horaInicio { get; set; }
+
+        [Required(ErrorMessage = "El campo hora de fin es obligatoria")]
+        [DataType(DataType.Time)]
+        public DateTime horaFin { get; set; }
+
+        public int idCurso { get; set; }
+        [ForeignKey(nameof(idCurso))]
+        public Curso? curso { get; set; }
+
+        public int idDocente { get; set; }
+        [ForeignKey(nameof(idDocente))]
+        public Docente? docente { get; set; }
+
+        public int idMateria { get; set; }
+        [ForeignKey(nameof(idMateria))]
+        public Materia? materia { get; set; }
+    }
+}
