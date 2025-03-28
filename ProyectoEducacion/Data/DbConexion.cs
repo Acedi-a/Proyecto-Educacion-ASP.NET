f1124452dc53fc1bfbf4d032844b2ca457bf4641
SergioWalker11 <romero75122443@gmail.com>
2025-03-27 22:06:27 -0400

    Se hizo DBconexion

---
+++ ProyectoEducacion/Data/DbConexion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
+using ProyectoEducacion.Modelos;
+namespace ProyectoEducacion.Data
+{
+	public class DbConexion:DbContext
+	{
+		public DbConexion(DbContextOptions<DbConexion> options) : base(options) { }
+		public DbSet<Asistencia> Asistencia { get; set;}
+		public DbSet<Calificacion> Calificacion { get; set;}
+		public DbSet<Certificado> Certificado { get; set;}
+		public DbSet<Curso> Curso { get; set;}
+		public DbSet<Docente> Docente { get; set;}
+		public DbSet<Estudiante> Estudiante { get; set;}
+		public DbSet<Examen> Examen { get; set;}
+		public DbSet<Horario> Horario { get; set;}
+		public DbSet<Materia> Materia { get; set;}
+		public DbSet<Mensaje> Mensaje { get; set;}
+		public DbSet<Notificacion> Notificacion { get; set;}
+		public DbSet<Pago> Pago { get; set;}
+		public DbSet<Reporte> Reporte { get; set;}
+		public DbSet<Usuario> Usuario { get; set;}
+
+	}
+}
